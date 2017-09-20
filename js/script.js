098f5c4da867efa7dc03ecb3661603a4c955b9d1
ADITI DWIVEDI <addoo96@gmail.com>
2017-09-21 03:08:30 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,27 @@
-// This is for List and Grid 
-
 listButton = $('button.list-view');
 gridButton = $('button.grid-view');
 wrapper = $('div.wrapper');
 
-
-
 listButton.on('click',function(){
-	
   gridButton.removeClass('on');
   listButton.addClass('on');
   wrapper.removeClass('grid').addClass('list');
-  
 });
 
 gridButton.on('click',function(){
-	
   listButton.removeClass('on');
   gridButton.addClass('on');
   wrapper.removeClass('list').addClass('grid');
-  
 });
-
-//END
-
+var total=0;
+    var counter = 1;
+	function myFunction(value, name)
+	{
+	if(counter<5){
+    	counter++;
+		total+=value;
+		console.log(total);
+		 document.getElementById("test").innerHTML=total;
+		 $(".product").append("<b>"+ name +"&emsp;&emsp;&emsp;</b>");
+		 $(".product").append(value +"<hr>");
+	}}
